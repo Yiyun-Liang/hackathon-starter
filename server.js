b4a578d2289a11cea55fb823b86a0fe3f13696ba
isa <isaliangyiyun@hotmail.com>
2016-05-23 19:35:06 -0400

    add mongodb

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express'),
-		stylus = require('stylus'),
-		logger = require('morgan'),
-		bodyParser = require('body-parser');
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
 function compile(str, path){
-	return stylus(str).set('filename', path);
+  return stylus(str).set('filename', path);
 }
 
 app.set('views', __dirname + '/server/views');
@@ -17,17 +18,41 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(stylus.middleware(
-	{
-		src: __dirname + '/public',
-		compile: compile
-	}
+{
+  src: __dirname + '/public',
+  compile: compile
+}
 ));
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/hackathon-starter'); //localhost/db name
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connecting error...'));
+db.once('open', function callback(){
+  console.log('hackathon-starter db opened!');
+});
+
+var userSchema = mongoose.Schema({
+  first_name: String,
+  last_name: String
+})
+
+var User = mongoose.model('User', userSchema);
+var mongoUser;
+User.findOne().exec(function(err, userDoc){
+  mongoUser = userDoc.first_name + userDoc.last_name;
+});
+
+app.get('/partials/:partialPath', function(req,res){
+  res.render('/partials/' + req.params.partialPath);
+});
+
 app.get('*', function(req, res){
-	res.render('index');
+  res.render('index', {
+    mongoUser : mongoUser
+  });
 });
 
 var port = 3030;
 app.listen(port)
-console.log("Server listening on port" + port);
+console.log("Server listening on port " + port + " ...");
