9694fb9b4ca73c6cc8b1c5890a449be1b0f5f56f
isa <isaliangyiyun@hotmail.com>
2016-05-23 14:07:54 -0400

    add stylus

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var express = require('express');
+var express = require('express'),
+		stylus = require('stylus'),
+		logger = require('morgan'),
+		bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+function compile(str, path){
+	return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res){
 	res.render('index');
